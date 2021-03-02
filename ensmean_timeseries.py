fa04f3e116244360b4daaad148557929777ed329
jwohland <jwohland@ethz.ch>
2021-03-02 16:50:09 +0100

    add picontrol maximum and plot improvements

--- ensmean_timeseries.py
+++ ensmean_timeseries.py
@@ -1,7 +1,11 @@
 from utils import *
 
+ds_picontrol = open_picontrol()
+max_pi = float(
+    ds_picontrol["sfcWind"].rolling(time=20).mean().dropna(dim="time").max()
+)
 
-f, ax = plt.subplots(ncols=4, figsize=(10, 5), sharey=True)
+f, ax = plt.subplots(ncols=4, figsize=(12, 4), sharey=True)
 # plot others
 for col_plot, experiment in enumerate(["historical", "rcp26", "rcp45", "rcp85"]):
     path = "../data/" + experiment + "/"
@@ -19,8 +23,9 @@ for col_plot, experiment in enumerate(["historical", "rcp26", "rcp45", "rcp85"])
         dim="ensemble_member"
     ).plot(ax=ax[col_plot], alpha=0.75, color="black")
     print("wind plotted")
-
+    ax[col_plot].axhline(max_pi, ls='--', label="PI control maximum")
     ax[col_plot].set_title(experiment)
 ax[0].set_ylabel("10m wind speed [m/s]", fontsize=15)
-plt.subplots_adjust(0.05, 0.05, 0.95, 0.9)
+ax[3].legend(loc="upper right")
+plt.tight_layout()
 plt.savefig("../plots/box_timeseries_Europe.jpeg", dpi=300)
