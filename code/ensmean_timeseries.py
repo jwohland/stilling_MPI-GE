ece730d79ca8f1c80c3e592b44400cbdb6a9126d
jwohland <jwohland@ethz.ch>
2021-08-11 14:27:04 +0200

    Added docstring

--- code/ensmean_timeseries.py
+++ code/ensmean_timeseries.py
@@ -6,7 +6,15 @@ from utils import open_picontrol, open_datasets
 
 
 def plot_ensemble_members_timeseries(path_to_data, path_to_plots):
-    # TODO: docstring
+    """
+    Plots 20y running mean timeseries (ensemble mean and ensemble members) of surface wind speeds for the
+    historical period and the 3 representative concentration pathways in the future.
+    For comparison with the pre-industrial period, a dashed line representing the maximum during the pre-industrial
+    period is added.
+    :param path_to_data: Path to directory that contains scenarios (historical, rcps) as subdirectories
+    :param path_to_plots: Path to plot directory
+    :return:
+    """
     ds_picontrol = open_picontrol(path_to_data)
     max_pi = float(
         ds_picontrol["sfcWind"].rolling(time=20).mean().dropna(dim="time").max()
